4fb15e2f36a02837f29c4ed352d7dcf3225225dc
harshavamsi <harshavamsi096@gmail.com>
2018-03-10 07:58:25 +0530

    Added plots

--- src/model.py
+++ src/model.py
@@ -1,129 +1,244 @@
-from pandas import DataFrame
-from pandas import Series
-from pandas import concat
-from pandas import read_csv
-from pandas import datetime
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from math import sqrt
-import matplotlib
-import numpy
-from numpy import concatenate
+import numpy as np
+import pandas as pd
+import math
+import sklearn
+import sklearn.preprocessing
+import datetime
+import os
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
-# date-time parsing function for loading the dataset
+# import all stock prices
+df = pd.read_csv("prices-split-adjusted.csv", index_col = 0)
+df.info()
+df.head()
 
-# frame a sequence as a supervised learning problem
-def timeseries_to_supervised(data, lag=1):
-	df = DataFrame(data)
-	columns = [df.shift(i) for i in range(1, lag+1)]
-	columns.append(df)
-	df = concat(columns, axis=1)
-	return df
+# number of different stocks
+print('\nnumber of different stocks: ', len(list(set(df.symbol))))
+print(list(set(df.symbol))[:10])
 
-# create a differenced series
-def difference(dataset, interval=1):
-	diff = list()
-	for i in range(interval, len(dataset)):
-		value = dataset[i] - dataset[i - interval]
-		diff.append(value)
-	return Series(diff)
+plt.figure(figsize=(15, 5));
+plt.subplot(1,2,1);
+plt.plot(df[df.symbol == 'EQIX'].open.values, color='red', label='open')
+plt.plot(df[df.symbol == 'EQIX'].close.values, color='green', label='close')
+plt.plot(df[df.symbol == 'EQIX'].low.values, color='blue', label='low')
+plt.plot(df[df.symbol == 'EQIX'].high.values, color='black', label='high')
+plt.title('crop price')
+plt.xlabel('time [days]')
+plt.ylabel('price')
+plt.legend(loc='best')
+#plt.show()
 
-# invert differenced value
-def inverse_difference(history, yhat, interval=1):
-	return yhat + history[-interval]
+plt.subplot(1,2,2);
+plt.plot(df[df.symbol == 'EQIX'].volume.values, color='black', label='volume')
+plt.title('crop volume')
+plt.xlabel('time [days]')
+plt.ylabel('volume')
+plt.legend(loc='best');
 
-# scale train and test data to [-1, 1]
-def scale(train, test):
-	# fit scaler
-	scaler = MinMaxScaler(feature_range=(-1, 1))
-	scaler = scaler.fit(train)
-	# transform train
-	train = train.reshape(train.shape[0], train.shape[1])
-	train_scaled = scaler.transform(train)
-	# transform test
-	test = test.reshape(test.shape[0], test.shape[1])
-	test_scaled = scaler.transform(test)
-	return scaler, train_scaled, test_scaled
+# function for min-max normalization of stock
+def normalize_data(df):
+    min_max_scaler = sklearn.preprocessing.MinMaxScaler()
+    df['open'] = min_max_scaler.fit_transform(df.open.values.reshape(-1,1))
+    df['high'] = min_max_scaler.fit_transform(df.high.values.reshape(-1,1))
+    df['low'] = min_max_scaler.fit_transform(df.low.values.reshape(-1,1))
+    df['close'] = min_max_scaler.fit_transform(df['close'].values.reshape(-1,1))
+    return df
 
-# inverse scaling for a forecasted value
-def invert_scale(scaler, X, yhat):
-	new_row = [x for x in X] + [yhat]
-	array = numpy.array(new_row)
-	array = array.reshape(1, len(array))
-	inverted = scaler.inverse_transform(array)
-	return inverted[0, -1]
+# function to create train, validation, test data given stock data and sequence length
+def load_data(stock, seq_len):
+    data_raw = stock.as_matrix() # convert to numpy array
+    data = []
 
-# fit an LSTM network to training data
-def fit_lstm(train, batch_size, nb_epoch, neurons):
-	X, y = train[:, 0:-1], train[:, -1]
-	X = X.reshape(X.shape[0], 1, X.shape[1])
-	model = Sequential()
-	model.add(LSTM(neurons, batch_input_shape=(batch_size, X.shape[1], X.shape[2]), stateful=True))
-	model.add(Dense(1))
-	model.compile(loss='mean_squared_error', optimizer='adam')
-	for i in range(nb_epoch):
-		model.fit(X, y, epochs=1, batch_size=batch_size, verbose=0, shuffle=False)
-		model.reset_states()
-	return model
+    # create all possible sequences of length seq_len
+    for index in range(len(data_raw) - seq_len):
+        data.append(data_raw[index: index + seq_len])
 
-# make a one-step forecast
-def forecast_lstm(model, batch_size, X):
-	X = X.reshape(1, 1, len(X))
-	yhat = model.predict(X, batch_size=batch_size)
-	return yhat[0,0]
+    data = np.array(data);
+    valid_set_size = int(np.round(40000/100*data.shape[0]));
+    test_set_size = int(np.round(20000/100*data.shape[0]));
+    train_set_size = data.shape[0] - (valid_set_size + test_set_size);
 
-# run a repeated experiment
-def experiment(repeats, series, features):
-	# transform data to be stationary
-	raw_values = series.values
-	diff_values = difference(raw_values, 1)
-	# transform data to be supervised learning
-	supervised = timeseries_to_supervised(diff_values, features)
-	supervised_values = supervised.values[features:,:]
-	# split data into train and test-sets
-	train, test = supervised_values[0:-12, :], supervised_values[-12:, :]
-	# transform the scale of the data
-	scaler, train_scaled, test_scaled = scale(train, test)
-	# run experiment
-	error_scores = list()
-	for r in range(repeats):
-		# fit the base model
-		lstm_model = fit_lstm(train_scaled, 1, 500, 1)
-		# forecast test dataset
-		predictions = list()
-		for i in range(len(test_scaled)):
-			# predict
-			X, y = test_scaled[i, 0:-1], test_scaled[i, -1]
-			yhat = forecast_lstm(lstm_model, 1, X)
-			# invert scaling
-			yhat = invert_scale(scaler, X, yhat)
-			# invert differencing
-			yhat = inverse_difference(raw_values, yhat, len(test_scaled)+1-i)
-			# store forecast
-			predictions.append(yhat)
-		# report performance
-		rmse = sqrt(mean_squared_error(raw_values[-12:], predictions))
-		print('%d) Test RMSE: %.3f' % (r+1, rmse))
-		error_scores.append(rmse)
-	return error_scores
+    x_train = data[:train_set_size,:-1,:]
+    y_train = data[:train_set_size,-1,:]
 
-# execute the experiment
-def run():
-	# load dataset
-	series = read_csv('../data/pricing/rice_final_3.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
-	# experiment
-	repeats = 1
-	results = DataFrame()
-	# run experiment
-	features = 1
-	results['results'] = experiment(repeats, series, features)
-	# summarize results
-	print(results.describe())
-	# save results
-	results.to_csv('experiment_features_1.csv', index=False)
+    x_valid = data[train_set_size:train_set_size+valid_set_size,:-1,:]
+    y_valid = data[train_set_size:train_set_size+valid_set_size,-1,:]
 
- # entry point
-run()
+    x_test = np.array(data[train_set_size+valid_set_size:,:-1,:])
+    y_test = np.array(data[train_set_size+valid_set_size:,-1,:])
+
+    return [x_train, y_train, x_valid, y_valid, x_test, y_test]
+
+# choose one stock
+df_stock = df[df.symbol == 'EQIX'].copy()
+df_stock.drop(['symbol'],1,inplace=True)
+df_stock.drop(['volume'],1,inplace=True)
+
+cols = list(df_stock.columns.values)
+print('df_stock.columns.values = ', cols)
+
+# normalize stock
+df_stock_norm = df_stock.copy()
+df_stock_norm = normalize_data(df_stock_norm)
+
+# create train, test data
+seq_len = 20 # choose sequence length
+x_train, y_train, x_valid, y_valid, x_test, y_test = load_data(df_stock_norm, seq_len)
+print('x_train.shape = ',x_train.shape)
+print('y_train.shape = ', y_train.shape)
+print('x_valid.shape = ',x_valid.shape)
+print('y_valid.shape = ', y_valid.shape)
+print('x_test.shape = ', x_test.shape)
+print('y_test.shape = ',y_test.shape)
+
+plt.figure(figsize=(15, 5));
+plt.plot(df_stock_norm.open.values, color='red', label='open')
+plt.plot(df_stock_norm.close.values, color='green', label='low')
+plt.plot(df_stock_norm.low.values, color='blue', label='low')
+plt.plot(df_stock_norm.high.values, color='black', label='high')
+#plt.plot(df_stock_norm.volume.values, color='gray', label='volume')
+plt.title('crop')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price/volume')
+plt.legend(loc='best')
+plt.show()
+
+## Basic Cell RNN in tensorflow
+
+index_in_epoch = 0;
+perm_array  = np.arange(x_train.shape[0])
+np.random.shuffle(perm_array)
+
+# function to get the next batch
+def get_next_batch(batch_size):
+    global index_in_epoch, x_train, perm_array
+    start = index_in_epoch
+    index_in_epoch += batch_size
+
+    if index_in_epoch > x_train.shape[0]:
+        np.random.shuffle(perm_array) # shuffle permutation array
+        start = 0 # start next epoch
+        index_in_epoch = batch_size
+
+    end = index_in_epoch
+    return x_train[perm_array[start:end]], y_train[perm_array[start:end]]
+
+# parameters
+n_steps = seq_len-1
+n_inputs = 4
+n_neurons = 200
+n_outputs = 4
+n_layers = 2
+learning_rate = 0.001
+batch_size = 50
+n_epochs = 100
+train_set_size = x_train.shape[0]
+test_set_size = x_test.shape[0]
+
+tf.reset_default_graph()
+
+X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_outputs])
+
+# use Basic RNN Cell
+layers = [tf.contrib.rnn.BasicRNNCell(num_units=n_neurons, activation=tf.nn.elu)
+          for layer in range(n_layers)]
+
+# use Basic LSTM Cell
+#layers = [tf.contrib.rnn.BasicLSTMCell(num_units=n_neurons, activation=tf.nn.elu)
+#          for layer in range(n_layers)]
+
+# use LSTM Cell with peephole connections
+#layers = [tf.contrib.rnn.LSTMCell(num_units=n_neurons,
+#                                  activation=tf.nn.leaky_relu, use_peepholes = True)
+#          for layer in range(n_layers)]
+
+# use GRU cell
+#layers = [tf.contrib.rnn.GRUCell(num_units=n_neurons, activation=tf.nn.leaky_relu)
+#          for layer in range(n_layers)]
+
+multi_layer_cell = tf.contrib.rnn.MultiRNNCell(layers)
+rnn_outputs, states = tf.nn.dynamic_rnn(multi_layer_cell, X, dtype=tf.float32)
+
+stacked_rnn_outputs = tf.reshape(rnn_outputs, [-1, n_neurons])
+stacked_outputs = tf.layers.dense(stacked_rnn_outputs, n_outputs)
+outputs = tf.reshape(stacked_outputs, [-1, n_steps, n_outputs])
+outputs = outputs[:,n_steps-1,:] # keep only last output of sequence
+
+loss = tf.reduce_mean(tf.square(outputs - y)) # loss function = mean squared error
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+training_op = optimizer.minimize(loss)
+
+# run graph
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for iteration in range(int(n_epochs*train_set_size/batch_size)):
+        x_batch, y_batch = get_next_batch(batch_size) # fetch the next training batch
+        sess.run(training_op, feed_dict={X: x_batch, y: y_batch})
+        if iteration % int(5*train_set_size/batch_size) == 0:
+            mse_train = loss.eval(feed_dict={X: x_train, y: y_train})
+            mse_valid = loss.eval(feed_dict={X: x_valid, y: y_valid})
+            print('%.2f epochs: MSE train/valid = %.6f/%.6f'%(
+                iteration*batch_size/train_set_size, mse_train, mse_valid))
+
+    y_train_pred = sess.run(outputs, feed_dict={X: x_train})
+    y_valid_pred = sess.run(outputs, feed_dict={X: x_valid})
+    y_test_pred = sess.run(outputs, feed_dict={X: x_test})
+
+y_train.shape
+ft = 0 # 0 = open, 1 = close, 2 = highest, 3 = lowest
+
+## show predictions
+plt.figure(figsize=(15, 5));
+plt.subplot(1,2,1);
+
+plt.plot(np.arange(y_train.shape[0]), y_train[:,ft], color='blue', label='train target')
+
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_valid.shape[0]), y_valid[:,ft],
+         color='gray', label='valid target')
+
+plt.plot(np.arange(y_train.shape[0]+y_valid.shape[0],
+                   y_train.shape[0]+y_test.shape[0]+y_test.shape[0]),
+         y_test[:,ft], color='black', label='test target')
+
+plt.plot(np.arange(y_train_pred.shape[0]),y_train_pred[:,ft], color='red',
+         label='train prediction')
+
+plt.plot(np.arange(y_train_pred.shape[0], y_train_pred.shape[0]+y_valid_pred.shape[0]),
+         y_valid_pred[:,ft], color='orange', label='valid prediction')
+
+plt.plot(np.arange(y_train_pred.shape[0]+y_valid_pred.shape[0],
+                   y_train_pred.shape[0]+y_valid_pred.shape[0]+y_test_pred.shape[0]),
+         y_test_pred[:,ft], color='green', label='test prediction')
+
+plt.title('past and future stock prices')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price')
+plt.legend(loc='best');
+
+plt.subplot(1,2,2);
+
+plt.plot(np.arange(y_train.shape[0], y_train.shape[0]+y_test.shape[0]),
+         y_test[:,ft], color='black', label='test target')
+
+plt.plot(np.arange(y_train_pred.shape[0], y_train_pred.shape[0]+y_test_pred.shape[0]),
+         y_test_pred[:,ft], color='green', label='test prediction')
+
+plt.title('future stock prices')
+plt.xlabel('time [days]')
+plt.ylabel('normalized price')
+plt.legend(loc='best');
+
+corr_price_development_train = np.sum(np.equal(np.sign(y_train[:,1]-y_train[:,0]),
+            np.sign(y_train_pred[:,1]-y_train_pred[:,0])).astype(int)) / y_train.shape[0]
+corr_price_development_valid = np.sum(np.equal(np.sign(y_valid[:,1]-y_valid[:,0]),
+            np.sign(y_valid_pred[:,1]-y_valid_pred[:,0])).astype(int)) / y_valid.shape[0]
+corr_price_development_test = np.sum(np.equal(np.sign(y_test[:,1]-y_test[:,0]),
+            np.sign(y_test_pred[:,1]-y_test_pred[:,0])).astype(int)) / y_test.shape[0]
+
+print('correct sign prediction for close - open price for train/valid/test: %.2f/%.2f/%.2f'%(
+    corr_price_development_train, corr_price_development_valid, corr_price_development_test))
+
